7b2976cbe61fe26d6a2b66b1629ab6922bfb4c0c
kylecods <klrotich417@gmail.com>
2021-03-27 21:44:38 +0300

    Revert "Back to less use of pointers."
    This reverts commit fe4dae3630fdbdaa9457c64d5dd14c093d9a85b9.

--- main.c
+++ main.c
@@ -5,34 +5,21 @@
 
 #include "vm/vm.h"
 
-int constr(ERAVM *vm, Values *args){
-    return 0;
-}
 
 int main(){
 
     u8 prog[] = { 
         // fun_name(1)..
-        OP_SPUSH, 0x00, 0xf0,
-//        OP_JUMP,8,//name of the function, or be the label...
-        OP_CPUSH,'+',
-//        OP_STORE, 0,
-//        OP_LOAD,0,
-//        OP_RET,
-//         OP_CPUSH, '3',
-
-        // OP_CPUSH, '/',
-        // OP_CPUSH, '%',
-        // OP_SPUSH, 0xff, 0xf0,
-//        OP_CPUSH,'-',
-//        OP_CALL,1,2,
-//        OP_STORE,1,
-//        OP_POP,
-        // OP_CONST, 0,
-        // OP_CONST, 1,
-        // OP_CONST, 2,
-        // OP_CONST, 3,
-        // OP_POP,    
+        OP_CPUSH, '+',
+        OP_CPUSH, '-',
+        OP_CPUSH, '/',
+        OP_CPUSH, '%',
+        OP_SPUSH, 0xff, 0xf0, 
+        OP_CONST, 0,
+        OP_CONST, 1,
+        OP_CONST, 2,
+        OP_CONST, 3,
+        OP_POP,    
         OP_HALT
     };
     ERAVM *vm = init_vm();
@@ -61,8 +48,11 @@ int main(){
         writebyte(vm, prog[i]);
         // vm->program_size +=1;
     }
-    TEMPLDCNST(vm,4004.5,0);
-  
+    // writebyte(vm, OP_PUSH);
+    // writebyte(vm, 3);
+    // writebyte(vm, OP_HALT);
+
+
     
     // load_file(vm, "assembler/test1.ebin");
     run(vm);
@@ -108,13 +98,13 @@ int main(){
     
     printf("program size: %d\n\n", vm->program_size);
 
-    puts("-------STACK-------\n");
+    puts(_RED"-------STACK-------\n"_RESET);
     //Dump stack for debugging purposes
-    for (int slot = 0; slot < vm->sp; slot++)
+    for (Values *slot = vm->stack; slot < vm->stackTop; slot++)
     {
-        printf("[ ");
-        PRINTVAL(vm->stack[slot]);
-        printf(" ]");
+        printf(_GREEN"[ ");
+        PRINTVAL(*slot);
+        printf(" ]"_RESET);
     }
     printf("\n");
     
--- vm/vm.h
+++ vm/vm.h
@@ -4,110 +4,34 @@
 #include "../era.h"
 
 
-
-typedef enum objType{
-    OBJ_CLASS,
-}ObjType;
-typedef struct obj
+typedef struct callframe
 {
-    ObjType type;
-    struct obj *next;
-}Obj;
-
-
-typedef struct objstring
-{
-    Obj obj;
-    u32 length;
-    u32 hash;
-    char *value;
-}ObjString;
-
-typedef enum methodType{
-    METHOD_NATIVE,
-    METHOD_NORMAL,
-}MethodType;
-
-typedef struct method{
-    MethodType type;
-    const char *name;
-    union
-    {
-    }as;
-}Method;
-
-
-typedef struct objclass
-{
-    Obj obj;
-    const char *name;
-    u32 numFields;
-    Method methods[METHOD_COUNT];
-}ObjClass;
-
-typedef struct frame
-{
-    u8 *return_point;
-    int fp;
-//    u32 nargs;
-    Values *locals;
-}Frame;
+    Values *fpointer;
+    u8 *ip;
+}CallFrame;
 
 typedef struct eravm{
-    ObjClass *objClass;
-
-     Obj *objects;
-
-    Frame frame;
+    CallFrame frames[FRAME_SIZE];
+    u32 frameCount;
+    //cant have globals store here
+    CallFrame *gframe;
+   
     u8 program[PROG_SIZE];
     u32 program_size;
-    int pc;
     u8 *pointer;
 
     //like constant pool in jvm: where large numbers and strings live
-    //NB: maybe make this dynamically allocated? heap?
+    //NB: maybe make this dynamically allocated?
     Values constant_pool[POOL_SIZE];
 
     //stack
     Values stack[STACK_SIZE];
-    int sp;
     Values *stackTop;
 
     //instructions
     u8 instr;
     bool state;
 }ERAVM;
-
-#define ALLOCATE_OBJ(vm, type, objType)\
-        (type*)allocate_object(vm, sizeof(type), objType)
-
-static Obj* allocate_object(ERAVM *vm, size_t size, ObjType type){
-  Obj* object = (Obj*)malloc(size);
-  object->type = type;
-  object->next = vm->objects;
-  vm->objects = object;
-  return object;
-}
-
-
-
-#define PUSH(vm, value) ((vm)->stack[(vm)->sp++] = (value))
-#define POP(vm) ((vm)->stack[--(vm)->sp])
-#define PEEK(vm) (((vm)->stack[(vm)->sp-1]))
-#define BINARY_OP(vm, op, valtype,type,more)\
-          do {\
-            type left = more(POP((vm)));\
-            type right = more(POP((vm)));\
-            PUSH((vm), valtype(left op right));\
-          } while(false);       \
-
-#define READ_BYTE(vm)  ((vm)->program[(vm)->pc++])
-#define READ_SHORT(vm) ((vm)->pc +=2, (u16)((vm)->program[(vm)->pc-2] << 8)|((vm)->program[(vm)->pc-1]))
-#define READ_CONST(vm) ((vm)->constant_pool[READ_BYTE((vm))])
-
-
-#define TEMPLDCNST(vm,value,position)\
-    (vm)->constant_pool[(position)] = FLOAT_VAL((value));\
 
 
 
@@ -117,15 +41,10 @@ typedef struct opt_instr{
     Opcodes type;
 }OPTINSTR;
 
-ERAVM* init_vm();
-Method* newMethod(ERAVM *vm, ObjClass *klass);
-ObjClass* newClass(ERAVM *vm,ObjClass *superClass,int numFields,const char *name);
+ERAVM *init_vm();
 void writebyte(ERAVM *vm, u8 byte);
 void load_file(ERAVM *vm, const char *path);
 void run(ERAVM *vm);
 void free_vm(ERAVM *vm);
-
-
-
 
 #endif
